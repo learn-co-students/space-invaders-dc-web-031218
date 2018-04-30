cd63b4a68fab612002b3df7c8bed6bbc1e5827d4
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2018-04-30 17:40:24 -0400

    Done.

--- crewMember.js
+++ crewMember.js
@@ -1,1 +1,37 @@
-class CrewMember {}
+class CrewMember {
+  constructor (position, ship) {
+    this.position = position;
+
+    if (ship) {
+      // let newShip = new Spaceship(ship);
+      this.currentShip = ship;
+    } else {
+      this.currentShip = "Looking for a Rig";
+    }
+  }
+
+
+  engageWarpDrive () {
+    if ((typeof this.currentShip === "object") && (this.position === "Pilot")) {
+       this.currentShip.warpDrive = "engaged";
+    } else {
+      return "had no effect";
+    }
+  }
+  setsInvisibility () {
+    //debugger;
+    if ((typeof this.currentShip === "object") && this.position === "Defender") {
+      // debugger;
+       this.currentShip.cloaked = true;
+    } else {
+      return "had no effect";
+    }
+  }
+  chargePhasers () {
+    if ((typeof this.currentShip === "object") && (this.position === "Gunner")) {
+       this.currentShip.phasersCharge = "charged";
+    } else {
+      return "had no effect";
+    }
+  }
+}
--- spaceship.js
+++ spaceship.js
@@ -1,1 +1,25 @@
-class Spaceship {}
+class Spaceship {
+  constructor (name, crew, numPhasers, numShieldLayers) {
+    this.name = name;
+    this.phasers = numPhasers;
+    this.shields = numShieldLayers;
+    this.cloaked = false;
+    this.warpDrive = "disengaged";
+
+    if (crew.length > 0) {
+      this.docked = false;
+      crew.forEach ( (member) => {
+        //debugger;
+        member.currentShip = this;
+      });
+      this.crew = crew;
+    } else {
+      this.docked = true;
+    }
+
+    this.phasersCharge = "uncharged";
+  }
+
+
+
+}

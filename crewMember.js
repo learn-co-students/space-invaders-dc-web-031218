aa170e5ad4b0a0cf022420e0785fd9bcc5179efe
jtorre20 <jtorres1989@gmail.com>
2018-05-01 14:52:14 -0400

    Done.

--- crewMember.js
+++ crewMember.js
@@ -1,1 +1,26 @@
-class CrewMember {}
+class CrewMember {
+  constructor(position) {
+    this.position = position;
+    this.currentShip = 'Looking for a Rig';
+    // this.currentShip = S
+  }
+  engageWarpDrive() {
+    if (this.currentShip == 'Looking for a Rig') {
+      return 'had no effect';
+    }
+  } 
+  setsInvisibility() {
+    if (this.currentShip === 'Looking for a Rig') {
+      return 'had no effect';
+    }else {
+      if (this.position === 'Defender') {
+        this.currentShip.cloaked = true;
+      }
+    }
+  }
+  chargePhasers() {
+    if (this.currentShip === 'Looking for a Rig') {
+      return 'had no effect';
+    }
+  }
+}

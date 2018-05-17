449626c47f3c1b96523528bfec3f4af3e4194d2f
github username <github email address>
2018-05-17 15:16:59 -0400

    Done.

--- spaceship.js
+++ spaceship.js
@@ -1,1 +1,28 @@
-class Spaceship {}
+class Spaceship {
+	constructor(name, crewMembers, phasers, shields){
+		this.name = name
+		this.phasers = phasers
+		this.shields = shields
+		this.crew = crewMembers
+		this.assignCrewToShip()
+		this.cloaked = false
+		this.warpDrive = "disengaged"
+		this.docked = true
+		this.phasersCharge = "uncharged"
+		if (crewMembers.length != 0){
+			this.docked = false
+		}
+	}
+
+	assignCrewToShip(){
+		this.crew.forEach(crewMember => crewMember.currentShip = this)
+	}
+	// cloaked(){
+	// 	//iterate through crewMembers to find defender
+	// 	let defender = crewMembers.filter(crewMember => crewMember.position === "defender")
+	// 	// //cloaked to true, if defender crewmember uses setsInvisibility, below was tried and did not work
+	// 	if(defender[0].setsInvisibility()){
+	// 		this.cloaked = true
+	// 	}
+	// }
+}
